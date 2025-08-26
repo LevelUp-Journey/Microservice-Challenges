4d36c424986b2a64b24d9afe91bbfef00c51fdd5
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-08-26 13:17:04 -0500

    chore: first commit

---
+++ src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
@@ -0,0 +1,13 @@
+package com.levelupjourney.microservicechallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceChallengesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceChallengesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelupjourney.microservicechallenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceChallengesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
