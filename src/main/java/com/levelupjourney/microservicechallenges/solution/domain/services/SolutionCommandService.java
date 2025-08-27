aee077ebb43b4b5b863bf31403a6014e289da5e0
JonatanFD <u20211b995@upc.edu.pe>
2025-08-27 18:00:52 -0500

    feat: add test command service

--- src/main/java/com/levelupjourney/microservicechallenges/solution/domain/services/SolutionCommandService.java
+++ src/main/java/com/levelupjourney/microservicechallenges/solution/domain/services/SolutionCommandService.java
@@ -1,4 +1,9 @@
 package com.levelupjourney.microservicechallenges.solution.domain.services;
 
+import com.levelupjourney.microservicechallenges.solution.domain.model.aggregates.Solution;
+import com.levelupjourney.microservicechallenges.solution.domain.model.commands.CreateSolutionCommand;
+import java.util.Optional;
+
 public interface SolutionCommandService {
-}vdf
+    public Optional<Solution> handle(CreateSolutionCommand command);
+}
