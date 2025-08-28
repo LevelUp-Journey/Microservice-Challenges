0a9dcbf064240aed2556be8070e48fa380373324
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-08-28 09:20:04 -0500

    feat: add TODO for future integration of Code Runner Microservice in SubmitSolutionCommand

--- src/main/java/com/levelupjourney/microservicechallenges/solution/domain/model/commands/SubmitSolutionCommand.java
+++ src/main/java/com/levelupjourney/microservicechallenges/solution/domain/model/commands/SubmitSolutionCommand.java
@@ -3,6 +3,9 @@ package com.levelupjourney.microservicechallenges.solution.domain.model.commands
 import com.levelupjourney.microservicechallenges.shared.domain.model.valueobjects.ChallengeId;
 import com.levelupjourney.microservicechallenges.shared.domain.model.valueobjects.StudentId;
 
+// For now this is a simple record
+// In the future, we will integrate the Code Runner Microservice here
+// TODO: Integrate Code Runner Microservice
 public record SubmitSolutionCommand(
         ChallengeId challengeId,
         StudentId studentId,
