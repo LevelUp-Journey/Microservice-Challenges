61537d1d35ce871562a5fbec5c3f5416f7abcc04
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-08-29 09:37:06 -0500

    feat: add Swagger UI URL output to application startup

--- src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
+++ src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
@@ -8,6 +8,7 @@ public class MicroserviceChallengesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceChallengesApplication.class, args);
+        System.out.println("SWAGGER: http://localhost:8080/swagger-ui/index.html" );
     }
 
 }
