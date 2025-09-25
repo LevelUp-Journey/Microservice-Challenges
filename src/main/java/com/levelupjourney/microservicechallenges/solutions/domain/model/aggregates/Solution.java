2bffd85b3082c84d75e2ec2af7a33a503e68eefd
JonatanFD <u20211b995@upc.edu.pe>
2025-09-25 18:32:23 -0500

    feat: enhance Solution and SolutionDetails to support creation from CreateSolutionCommand

--- src/main/java/com/levelupjourney/microservicechallenges/solutions/domain/model/aggregates/Solution.java
+++ src/main/java/com/levelupjourney/microservicechallenges/solutions/domain/model/aggregates/Solution.java
@@ -1,17 +1,42 @@
 package com.levelupjourney.microservicechallenges.solutions.domain.model.aggregates;
 
 import com.levelupjourney.microservicechallenges.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import com.levelupjourney.microservicechallenges.solutions.domain.model.commands.CreateSolutionCommand;
 import com.levelupjourney.microservicechallenges.solutions.domain.model.valueobjects.*;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.UUID;
 
 @Getter
+@NoArgsConstructor
 @Entity
 public class Solution extends AuditableAbstractAggregateRoot<Solution> {
-    private SolutionId id;
+    
+    @Id
+    private UUID id;
+    
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "challenge_id"))
     private ChallengeId challengeId;
+    
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "code_version_id"))
     private CodeVersionId codeVersionId;
+    
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "student_id"))
     private StudentId studentId;
+    
+    @Embedded
     private SolutionDetails details;
+
+    public Solution(CreateSolutionCommand command) {
+        this.id = UUID.randomUUID();
+        this.challengeId = command.challengeId();
+        this.codeVersionId = command.codeVersionId();
+        this.studentId = command.studentId();
+        this.details = new SolutionDetails(command.code());
+    }
 }
--- src/main/java/com/levelupjourney/microservicechallenges/solutions/domain/model/valueobjects/SolutionDetails.java
+++ src/main/java/com/levelupjourney/microservicechallenges/solutions/domain/model/valueobjects/SolutionDetails.java
@@ -1,10 +1,33 @@
 package com.levelupjourney.microservicechallenges.solutions.domain.model.valueobjects;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Embeddable
 public class SolutionDetails {
+    
     private Integer attempts;
+
+    @NotNull
+    @Setter
     private String code;
+
+    @Temporal(TemporalType.TIMESTAMP)
     private Date lastAttemptAt;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
     private SolutionStatus status;
+
+    public SolutionDetails(String code) {
+        this.attempts = 0;
+        this.status = SolutionStatus.NO_TESTED;
+        this.code = code;
+    }
 }
