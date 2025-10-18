d2f89e1c2c25d3d7209d1b41a8ae39276bbf921b
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-10-17 21:38:11 -0500

    feat: enhance application startup to dynamically retrieve server port for Swagger UI

--- src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
+++ src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
@@ -2,6 +2,7 @@ package com.levelupjourney.microservicechallenges;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -9,8 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class MicroserviceChallengesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroserviceChallengesApplication.class, args);
-        System.out.println("SWAGGER: http://localhost:8082/swagger-ui/index.html" );
+        ConfigurableApplicationContext context = SpringApplication.run(MicroserviceChallengesApplication.class, args);
+        String port = context.getEnvironment().getProperty("server.port", "8080");
+        System.out.println("SWAGGER: http://localhost:" + port + "/swagger-ui/index.html");
     }
 
 }
