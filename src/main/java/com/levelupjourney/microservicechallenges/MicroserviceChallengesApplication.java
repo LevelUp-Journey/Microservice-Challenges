c8f5c17ee2a81413d37d2c0f286582c9ff7d3079
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-11-02 06:32:39 -0500

    feat: adding service discovery configuration and fix .env read

--- src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
+++ src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
@@ -1,5 +1,6 @@
 package com.levelupjourney.microservicechallenges;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,9 +13,23 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class MicroserviceChallengesApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(MicroserviceChallengesApplication.class, args);
-        String port = context.getEnvironment().getProperty("server.port", "8080");
-        System.out.println("SWAGGER: http://localhost:" + port + "/swagger-ui/index.html");
-    }
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv
+            .entries()
+            .forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+            );
 
+        ConfigurableApplicationContext context = SpringApplication.run(
+            MicroserviceChallengesApplication.class,
+            args
+        );
+        String port = context
+            .getEnvironment()
+            .getProperty("server.port", "8080");
+        System.out.println(
+            "SWAGGER: http://localhost:" + port + "/swagger-ui/index.html"
+        );
+    }
 }
