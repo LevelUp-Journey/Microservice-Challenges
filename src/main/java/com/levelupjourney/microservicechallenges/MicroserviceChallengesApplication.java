3c800dda24b6f969fd33dc6973340ecb8fcf6e50
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-11-02 05:38:40 -0500

    feat: Add Spring Cloud dependencies and configure Eureka service discovery

--- src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
+++ src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
@@ -2,11 +2,13 @@ package com.levelupjourney.microservicechallenges;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class MicroserviceChallengesApplication {
 
     public static void main(String[] args) {
