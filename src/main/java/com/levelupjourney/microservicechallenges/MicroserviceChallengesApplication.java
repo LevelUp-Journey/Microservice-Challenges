bc8fded69a3f59c131dfb57831cfeeaaf117f9fe
JonatanFD <u20211b995@upc.edu.pe>
2025-11-04 19:28:21 -0500

    feat: removing bad conf for dotenv

--- src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
+++ src/main/java/com/levelupjourney/microservicechallenges/MicroserviceChallengesApplication.java
@@ -1,10 +1,8 @@
 package com.levelupjourney.microservicechallenges;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -13,23 +11,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class MicroserviceChallengesApplication {
 
     public static void main(String[] args) {
-        // Load environment variables from .env file
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv
-            .entries()
-            .forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-            );
 
-        ConfigurableApplicationContext context = SpringApplication.run(
+        SpringApplication.run(
             MicroserviceChallengesApplication.class,
             args
         );
-        String port = context
-            .getEnvironment()
-            .getProperty("server.port", "8080");
-        System.out.println(
-            "SWAGGER: http://localhost:" + port + "/swagger-ui/index.html"
-        );
+
     }
 }
